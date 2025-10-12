425719ae32594e37cea43c7c089abf226f18ea3d
linwu <linwu@vv.cn>
2025-10-12 23:56:48 +0800

    config: 关闭 TypeScript 和 React Hooks 相关的 ESLint 规则

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -23,8 +23,9 @@ const eslintConfig = [
   {
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-unused-vars": "warn",
-      "react-hooks/exhaustive-deps": "warn",
+      "@typescript-eslint/no-unused-vars": "off",
+      "react-hooks/exhaustive-deps": "off",
+      "@typescript-eslint/no-unused-expressions": "off",
     },
   },
 ];
